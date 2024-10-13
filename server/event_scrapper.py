25c68e5662cdfd97e440b47faeeebefa73d6c9ea
Robel Schwarz <itsrobel.schwarz@gmail.com>
2024-10-12 18:53:53 -0700

    added flask http and server

--- server/event_scrapper.py
+++ server/event_scrapper.py
@@ -10,14 +10,8 @@ print("hello")
 driver.get(events_dict["Football"])
 
 title = driver.title
-
 driver.implicitly_wait(0.5)
 
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
-text_box.send_keys("Selenium")
-submit_button.click()
 
 message = driver.find_element(by=By.ID, value="message")
 text = message.text
