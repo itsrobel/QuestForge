1fbbce335f25709b0c17d26b0810b300a48d7826
Ruslana Korolov <rkorolov@icloud.com>
2024-10-12 20:05:42 -0700

    Added Profile to Stack Navigator

--- native/App.js
+++ native/App.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
+import Profile from './screens/Profile';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +17,9 @@ export default function App() {
           component={HomeScreen}
           options={{ title: 'Welcome' }}
         />
+
+        <Stack.Screen name="Profile" component={Profile}/>
+
       </Stack.Navigator>
       {/* <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
--- native/screens/HomeScreen.js
+++ native/screens/HomeScreen.js
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
+import Profile from './Profile';
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text>Home Screen</Text>
-      {/* <Button
+      <Button
         title="Go to Profile"
         onPress={() => navigation.navigate('Profile')}
-      /> */}
+      />
     </View>
   );
 };
