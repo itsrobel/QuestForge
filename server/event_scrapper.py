bcfd7ab9d946c1b98e650a9f6a7c3120d8ece0a1
Robel Schwarz <itsrobel.schwarz@gmail.com>
2024-10-12 19:51:05 -0700

    added webscrapper

--- server/event_scrapper.py
+++ server/event_scrapper.py
@@ -1,19 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import requests
 
 events_dict = {"Football": "https://gohuskies.com/sports/football/schedule/2024"}
 
 
-driver = webdriver.Chrome()
-print("hello")
-
-driver.get(events_dict["Football"])
-
-title = driver.title
-driver.implicitly_wait(0.5)
+page = requests.get(events_dict["Football"])
 
 
-message = driver.find_element(by=By.ID, value="message")
-text = message.text
+# driver = webdriver.Chrome()
 
-driver.quit()
+# driver.get(events_dict["Football"])
+
+# title = driver.title
+# driver.implicitly_wait(0.5)
+
+
+# soup = driver.page_source
+soup = BeautifulSoup(page.content, "html.parser")
+games = {}
+
+
+coming_games = soup.find_all(class_="upcoming-game")
+for game in coming_games:
+    date = game.find(class_="sidearm-schedule-game-opponent-date").text.strip()
+    name = game.find(class_="sidearm-schedule-game-opponent-name").text.strip()
+    location = game.find(class_="sidearm-schedule-game-location").text.strip()
+    games[date] = {"team": name, "location": location}
+print(games)
