8c4c89c1c91ae10fce22051c1dc0b1da48da1605
Ruslana Korolov <rkorolov@icloud.com>
2024-10-12 19:58:51 -0700

    Create Profile.js

---
+++ native/screens/Profile.js
@@ -0,0 +1,16 @@
+import * as React from 'react-native';
+import { View, Text, Button } from 'react-native';
+
+const Profile = ({ navigation }) => {
+    return (
+        <View>
+        <Text>Profile Screen</Text>
+        {/* <Button
+            title="Go to Home"
+            onPress={() => navigation.navigate('Home')}
+        /> */}
+        </View>
+    );
+}
+
+export default Profile;
